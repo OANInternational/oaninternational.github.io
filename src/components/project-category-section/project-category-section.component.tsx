8a05527b898d25ec271a9caf0ebf4b10d6cea858
Alfonso Garcia <alfonso.garcia@remberg.de>
2023-10-28 16:08:20 +0200

    responsive

---
+++ src/components/project-category-section/project-category-section.component.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+
+import styles from "./project-category-section.module.css";
+import ProjectPreview, {
+  IProjectPreview,
+} from "../project-preview/project-preview.component";
+import { ReactElement } from "react";
+
+export interface IProjectCategory {
+  title: string;
+  icon: ReactElement;
+  url: string;
+  description: ReactElement;
+  backgroundColor: string;
+  backgroundColorLight: string;
+  projects: IProjectPreview[];
+}
+
+export default function ProjectCategorySection(props: {
+  projectCategory: IProjectCategory;
+}) {
+  const projects = props.projectCategory.projects.map((project) => (
+    <ProjectPreview key={project.title} project={project} />
+  ));
+
+  const titleSectionStyle = {
+    backgroundColor: props.projectCategory.backgroundColor,
+  };
+
+  const titleTextSectionStyle = {
+    backgroundColor: props.projectCategory.backgroundColorLight,
+  };
+
+  return (
+    <div>
+      <section className={styles.projectsSection}>
+        <div className={styles.projectTitleSection} style={titleSectionStyle}>
+          <h2
+            className={styles.projectTitleTextSection}
+            style={titleTextSectionStyle}
+          >
+            {props.projectCategory.title}
+          </h2>
+        </div>
+
+        <div className={styles.projectCategoryDescription}>
+          {props.projectCategory.description}
+        </div>
+
+        <div>{projects}</div>
+      </section>
+    </div>
+  );
+}
