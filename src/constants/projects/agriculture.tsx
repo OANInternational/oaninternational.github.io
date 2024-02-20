71a576cddedb8dd12b57f8e79053f914c5ac1967
Alfonso Garcia <alfonso.garcia@remberg.de>
2024-02-20 19:35:40 +0100

    update project texts

--- src/constants/projects/agriculture.tsx
+++ src/constants/projects/agriculture.tsx
@@ -7,27 +7,38 @@ const wrapper: CSSProperties = {
   gap: "20px",
 };
 
+const link: CSSProperties = {
+  color: "black",
+};
+
 export const AGRICULTURE_PROJECTS: IProjectPreview[] = [
   {
-    title:
-      "Empoderamiento de cooperativas de producción agrícola en la comuna Nikki",
+    title: "Nikarit",
     summary: (
       <div style={wrapper}>
         <p>
-          Mejora de los conocimientos agrícolas de las personas miembro de las
-          cooperativas mediante formaciones en técnicas agrícolas,
-          sensibilización en el uso de recursos hídricos, y en el cuidado de la
-          tierra de cultivo. Mejora del acceso a semillas en la ciudad de Nikki.
+          Proyecto de emprendimiento social que busca{" "}
+          <b>empoderar a mujeres productoras</b> de manteca de karité de Nikki
+          para que a través de la venta de este producto puedan mejorar su vida
+          y la de sus comunidades. Para ello, acompañamos a 6 cooperativas de
+          mujeres, en el refuerzo de capacidades y en el acceso a herramientas
+          para que puedan producir manteca de karité de alta calidad y mejoren
+          su acceso al mercado nacional e internacional.
         </p>
-      </div>
-    ),
-    description: (
-      <div style={wrapper}>
+
         <p>
-          Mejora de los conocimientos agrícolas de las personas miembro de las
-          cooperativas mediante formaciones en técnicas agrícolas,
-          sensibilización en el uso de recursos hídricos, y en el cuidado de la
-          tierra de cultivo. Mejora del acceso a semillas en la ciudad de Nikki.
+          La manteca de karité se transforma después en productos cosméticos que
+          puedes comprar aquí{" "}
+          <a style={link} href="https://nikarit.es/">
+            (nikarit.es)
+          </a>{" "}
+          y con estos ingresos apoyamos la financiación del resto de proyectos.
+        </p>
+
+        <p>
+          <a style={link} href="https://www.instagram.com/nikarit_karite/">
+            instagram.com/nikarit_karite/
+          </a>
         </p>
       </div>
     ),
@@ -35,28 +46,38 @@ export const AGRICULTURE_PROJECTS: IProjectPreview[] = [
     url: "",
   },
   {
-    title: "Investigaciones estratégicas",
+    title: "Huertas comunitarias",
     summary: (
       <div style={wrapper}>
         <p>
-          Desde la Universidad de Parakou, se alertó a OAN International de la
-          posibilidad real de que en el medio- largo plazo este árbol
-          desaparezca debido al uso abusivo de sus semillas. El objetivo general
-          de esta investigación fue estudiar la situación del árbol del karité
-          en el norte de Benín para poder identificar los retos a los que se
-          enfrenta y posibles soluciones.
+          Este Proyecto busca <b>apoyar a grupos de mujeres rurales</b> para que
+          desarrollen huertas comunitarias en sus pueblos que les permitan tener
+          acceso a alimentos variados, y poder vender los excedentes para
+          conseguir una fuente de ingresos adicional.
         </p>
       </div>
     ),
-    description: (
+    imageUrl: "/oan-web-004.jpg",
+    url: "",
+  },
+  {
+    title: "Microcréditos para mujeres",
+    summary: (
       <div style={wrapper}>
         <p>
-          Desde la Universidad de Parakou, se alertó a OAN International de la
-          posibilidad real de que en el medio- largo plazo este árbol
-          desaparezca debido al uso abusivo de sus semillas. El objetivo general
-          de esta investigación fue estudiar la situación del árbol del karité
-          en el norte de Benín para poder identificar los retos a los que se
-          enfrenta y posibles soluciones.
+          Este proyecto apoya a un grupo de mujeres emprendedoras en la ciudad
+          de Nikki, facilitándoles el acceso a microcréditos con los que pueden
+          impulsar sus pequeños negocios, y creado espacios de aprendizaje
+          colectivo donde comparten experiencias, se apoyan para resolver sus
+          problemas, se forman con personas especialistas, y ponen en marcha
+          proyectos colectivos.
+        </p>
+
+        <p>
+          En este proyecto también se apoya a estas mujeres para la celebracion
+          de la jornada mundial de la mujer y de la jornada de lucha contra la
+          violencia hacia las mujeres. Siendo estos dos momentos en los que
+          ellas lideran la sensibilización en la comunidad sobre estos temas.
         </p>
       </div>
     ),
--- src/constants/projects/categories.tsx
+++ src/constants/projects/categories.tsx
@@ -25,38 +25,39 @@ const list: CSSProperties = {
 export const PROJECT_CATEGORIES: IProjectCategory[] = [
   {
     id: "agriculture",
-    title: "Agricultura",
+    title: "Mujer y medios de vida sostenibles",
     icon: <TbPlant size={100} />,
     url: "/proyectos/agricultura",
     description: (
       <div style={wrapper}>
         <p>
-          En el caso concreto de Nikki, el acceso a los productos hortícolas es
-          deficiente y no continuado a lo largo del año, ya que los trabajos en
-          las huertas se realizan únicamente entre los meses de febrero y junio,
-          siendo la época de producción muy corta.
+          Para que las comunidades sean capaces de acceder de manera autónoma a
+          los servicios básicos como el agua, la salud o la educación, es
+          imprescindible que tengan un medio de vida que les haga ser
+          independientes económicamente.
         </p>
 
         <p>
-          Actualmente, OAN International trabaja a través de este programa en el
-          acompañamiento a cooperativas productoras de la zona, estableciendo
-          como objetivos generales:
+          En este sentido, las mujeres juegan un papel esencial para el
+          desarrollo de sus comunidades, ya que tradicionalmente en Nikki, son
+          las encargadas de la educación, la alimentación y la salud de sus
+          hijos. Sin embargo, las mujeres tienen más barreras para acceder a la
+          educación y al mercado de trabajo, lo que favorece los matrimonios
+          precoces, la violencia doméstica.
         </p>
 
-        <ul style={list}>
-          <li>
-            Mejorar los niveles de seguridad alimentaria en la comuna de Nikki.
-            Se engloba dentro del ODS 2: poner fin al hambre, logrando la
-            seguridad alimentaria y la mejora de la nutrición promoviendo la
-            agricultura sostenible en la comuna
-          </li>
-          <li>
-            Promover la igualdad de género. Cumple con el ODS 5: lograr la
-            igualdad entre los géneros y empoderar a las mujeres. A través de la
-            formación agrícola y el enfoque participativo se pretende fortalecer
-            y visibilizar el papel de la mujer en la agricultura.
-          </li>
-        </ul>
+        <p>
+          Por ello, favorecer la capacitación y el trabajo digno de las mujeres
+          permite romper con esas barreras y mejorar no solo su propia vida sino
+          la de toda su comunidad.
+        </p>
+
+        <p>
+          En este conexto, promovemos la autosuficiencia económica de las
+          mujeres en Nikki, en línea con el ODS 1 (fin de la pobreza), el ODS 5
+          (lograr la igualdad de género), y el ODS 7 (trabajo digno y
+          crecimiento económico).
+        </p>
       </div>
     ),
     backgroundColor: "var(--agriculture-color)",
@@ -65,7 +66,7 @@ export const PROJECT_CATEGORIES: IProjectCategory[] = [
   },
   {
     id: "heath",
-    title: "Salud",
+    title: "Nutrición",
     icon: <TbHealthRecognition size={100} />,
     url: "/proyectos/salud",
     backgroundColor: "var(--health-color)",
@@ -73,126 +74,41 @@ export const PROJECT_CATEGORIES: IProjectCategory[] = [
     description: (
       <div style={wrapper}>
         <p>
-          La Asamblea General de Naciones Unidas adoptó en 2015 los Objetivos de
-          Desarrollo Sostenible (ODS). El objetivo 3 es: “Garantizar una vida
-          sana y promover el bienestar para todos en todas las edades”, y
-          concierne directamente a la salud. Benín se suscribió a los ODS y ha
-          optado por una política para la cobertura sanitaria universal de su
-          población a través del refuerzo de su sistema de salud y la promoción
-          de los cuidados de salud primarios. La Estrategia de Cooperación de la
-          Organización Mundial de la Salud (OMS) en Benín 2016-2019 tiene 4
-          prioridades estratégicas:
+          El derecho a la salud es un derecho humano reconocido entre otros en
+          el
         </p>
-
-        <ul style={list}>
-          <li>Acelerar la reducción de carga de enfermedades transmisibles.</li>
-
-          <li>
-            Apoyar el desarrollo y la puesta en marcha de políticas sectoriales,
-            estrategias y programas que sirvan para prevenir y controlar las
-            enfermedades no transmisibles y sus factores de riesgo.
-          </li>
-
-          <li>
-            Mejorar los indicadores de salud a lo largo de la vida y promover
-            comportamientos sanos.
-          </li>
-
-          <li>
-            Aportar un apoyo técnico de calidad al país para poder permitirle
-            dotarse de un sistema de salud, capaz de responder a las necesidades
-            de la población en materia de cuidados sanitarios de calidad,
-            promover los derechos humanos, la equidad de género y la resiliencia
-            en caso de urgencia.
-          </li>
-        </ul>
 
         <p>
-          Una importante característica de Benín es la marcada y creciente
-          desigualdad entre regiones. La región del norte es aquella que
-          presenta mayores tasas de pobreza y los indicadores sanitarios más
-          dramáticos. De este modo, desde OAN International, y gracias a las
-          investigaciones de diversos TFG, se establecen como prioridades:
+          La FAO considera a la República de Benín uno de los 51 Países de Bajos
+          Ingresos y Déficit de Alimentos (PBIDA). La malnutrición es un
+          problema generalizado, y la población infantil se ve especialmente
+          afectada. A nivel nacional, un preocupante 30% de los niños (más de
+          1,1 millones de niños) sufren desnutrición crónica. Incluso hoy, más
+          de un tercio de los niños menores de 5 años sufren retraso del
+          crecimiento por falta de una ingesta nutricional adecuada. Además, el
+          75% de los niños no siguen una dieta equilibrada.
         </p>
 
-        <ol style={list}>
-          <li>Luchar contra la malaria</li>
-          <li>Mejora de la salud materno-infantil</li>
-          <li>Luchar contra la malnutrición</li>
-        </ol>
+        <p>
+          El departamento de Borgou registra la mayor prevalencia de
+          insuficiencia ponderal de Benín, y en la zona de salud de
+          Nikki-Kalalé-Pèrèrè (ZSNKP), la desnutrición infantil es la tercera
+          causa de hospitalización y la segunda de muerte entre los menores de 5
+          años.
+        </p>
+
+        <p>
+          En este contexto, en OAN trabajamos para mejorar la nutrición materno
+          infantil en Nikki en línea con los ODS 2 (Hambre cero) y 3 (Salud y
+          bienestar).
+        </p>
       </div>
     ),
     projects: HEALTH_PROJECTS,
   },
   {
-    id: "social",
-    title: "Políticas Sociales",
-    icon: <PiHandshakeLight size={100} />,
-    url: "/proyectos/politicassociales",
-    backgroundColor: "var(--social-color)",
-    backgroundColorLight: "var(--social-color-light)",
-    description: (
-      <div style={wrapper}>
-        <p>
-          La Agenda 2030 de la ONU establece los Objetivos de Desarrollo
-          Sostenible (ODS) que buscan el trabajo conjunto de todos los estados
-          hacia una sociedad más justa e igualitaria. En ese contexto, los
-          esfuerzos del Comité de Políticas Sociales se enmarcan dentro de los
-          objetivos 1, fin de la pobreza, y 5, cuya meta es lograr la igualdad
-          de género, especialmente importante para alcanzar el resto de ODS. Del
-          mismo modo, y para lograr un desarrollo sostenible, la Agenda Africana
-          2063 propone alcanzar la igualdad de género en todas las esferas de la
-          vida; eliminando cualquier tipo de discriminación en contra de las
-          mujeres, ya sea a nivel social, cultural, político o económico; y
-          logrando la paridad en las administraciones antes del año 2020. Esto,
-          junto con el Decenio de la Mujer Africana, declarado entre los años
-          2010-2020, muestran un cambio y compromiso en la hoja de ruta de los
-          estados africanos en relación a la situación de sus mujeres.
-        </p>
-        <p>
-          La lucha contra la pobreza está estrechamente vinculada a la lucha por
-          la igualdad de género y el empoderamiento de la mujer, ya que la
-          pobreza es sexista. Conceptualizar la pobreza desde el enfoque de
-          género supone reconocer que hombres y mujeres no se ven afectados de
-          la misma manera por este fenómeno.
-        </p>
-
-        <p>
-          De los aproximadamente 11.000.000 habitantes que se contabilizan en
-          Benín, alrededor de 5.200.000 son mujeres, de las cuales a su vez
-          3.000.000 tendrían menos de 25 años. En el caso de Nikki, y según
-          datos de 2010, de 99.251 habitantes estimados, 49.619 serían mujeres,
-          frente a 49.632 hombres.
-        </p>
-
-        <p>
-          Se podría afirmar grosso modo que la sociedad presenta una clara
-          dicotomía en el espacio público-privado. Mientras que los hombres son
-          los protagonistas en los espacios públicos, las mujeres son relegadas
-          al espacio doméstico. Además, se da una división sexual del trabajo,
-          llegando a haber incluso dos economías: el hombre es el encargado en
-          el hogar de proporcionar alimentación a sus mujeres e hijos; la mujer
-          carga con los gastos en salud y educación, gestionando sus propios
-          ingresos. El abanico de actividades es amplio, desde la venta de
-          comida en el mercado, al comercio de los excedentes del cultivo,
-          pasando por el transporte de leña o la cría de animales. A este
-          trabajo hay que sumarle el tiempo empleado en ir a buscar agua y
-          combustible, llegando a invertir hasta 3 horas en la preparación de la
-          comida de la familia.
-        </p>
-
-        <p>
-          Son por todos estos motivos, que no solo los proyectos del Comité de
-          Políticas Sociales, sino todos los proyectos de OAN International,
-          incorporan la variable de género.
-        </p>
-      </div>
-    ),
-    projects: SOCIAL_PROJECTS,
-  },
-  {
     id: "engineering",
-    title: "Energía, agua e infraestructura",
+    title: "Acceso al agua",
     icon: <RiWaterFlashLine size={100} />,
     url: "/proyectos/energia",
     backgroundColor: "var(--energy-color)",
@@ -200,68 +116,29 @@ export const PROJECT_CATEGORIES: IProjectCategory[] = [
     description: (
       <div style={wrapper}>
         <p>
-          Los principales servicios involucrados en los servicios básicos son el
-          agua, el saneamiento y la gestión de residuos. Según la OMS, en 2018
-          más de 1.000 millones de personas en el mundo no tienen acceso a agua
-          potable, y 2.600 millones carecen de un saneamiento adecuado. En
-          concreto, se estima que 660 millones de africanos no tienen acceso a
-          servicios de saneamiento adecuado, y 173 millones todavía practican la
-          defecación al aire libre (WWC, 2018).
+          El acceso al agua es la primera condición indispensable para la vida y
+          para el desarrollo.
         </p>
         <p>
-          El agua y el saneamiento son fundamentales para el desarrollo y el
-          bienestar humano, y están reconocidos como derechos humanos por la
-          Asamblea General de las Naciones Unidas (2010). Sin embargo, también
-          son fundamentales para el buen desarrollo de otros sectores, como la
-          nutrición adecuada, la educación y la erradicación de la pobreza .
+          El acceso al agua es un derecho humano reconocido de forma explícita
+          por la Asamblea de las Naciones Unidas a través de la Resolución
+          64/292 en 2010. Sin embargo, desgraciadamente en Nikki aún una gran
+          parte de la población carece de acceso al agua o tiene un acceso
+          deficiente.
         </p>
 
         <p>
-          El agua es uno de los recursos naturales fundamentales y en la
-          actualidad no existe duda alguna de la importancia tanto de la
-          cantidad como de la calidad de la misma para el bienestar y el
-          desarrollo humano. Se necesitan entre 50 y 100 litros/persona diarios
-          para cubrir cómodamente todas las necesidades de consumo e higiene,
-          estando el mínimo necesario alrededor de los 25. En 2010, la Asamblea
-          General de las Naciones Unidas reconoció explícitamente el derecho
-          humano al abastecimiento de agua dulce. Todas las personas tienen
-          derecho a disponer de forma continuada de agua suficiente, salubre,
-          físicamente accesible, asequible y de una calidad aceptable, para uso
-          personal y doméstico (ONU, 2010). Se trata de un recurso abundante y
-          renovable, sin embargo, aún en la actualidad existe una gran cantidad
-          de personas que no disponen de acceso suficiente a agua apropiada para
-          su consumo.
+          En OAN trabajamos para buscar <b>soluciones innovadoras</b> que
+          faciliten el acceso al agua a la población de Nikki.
         </p>
 
         <p>
-          Los datos revelan que la mayor parte de la población sin acceso a agua
-          potable en el entorno mundial se sitúa en África subsahariana, con 319
-          millones de personas, de las que 8 de cada 10 viven en zonas rurales
-          con escaso acceso a nuevos recursos.
-        </p>
-
-        <p>
-          Por otro lado, a pesar de considerar la electricidad como una
-          necesidad básica y pilar en la vida de todo país o comunidad, en
-          África, más de 600 millones de personas, casi 2/3 de la población,
-          viven sin suministro eléctrico. Millones de familias y hogares
-          dependen de fuentes de energía tradicionales que se traducen en
-          tecnologías contaminantes, costosas, y lo que es más importante,
-          negativas para la salud humana. Esta situación supone, sin duda, el
-          impedimento fundamental para el avance y desarrollo de estos países.
-        </p>
-
-        <p>
-          En concreto en Benín, el 85 % de la electricidad proviene de Ghana,
-          Nigeria y Costa de Marfil y los productos petrolíferos consumidos son,
-          en su totalidad, de origen externo. Esta situación de dependencia tan
-          elevada hace a Benín un país muy vulnerable a cualquier tipo de
-          fluctuación que sufran sus países vecinos. Debido a ello, el consumo
-          de energía en este país es, aproximadamente, la mitad del de los
-          países de África Subsahariana y menos de una cuarta parte de la media
-          mundial. Estos datos se ven reflejados en la tasa de electrificación,
-          que supone solamente un 27 %, siendo el de las zonas urbanas un 54 % y
-          el de las rurales un 4 %.
+          Esta actuación se enmarca denteo del ODS 6 (Agua limpia y saneamiento)
+          y se alinea con los Planes de Desarrollo Comunitario de Nikki (PDC
+          2017-2021) que incluyen el objetivo específico de “mejorar los
+          servicios de aprovisionamiento de agua, saneamiento y protección del
+          medioambiente”, siendo el resultado esperado “el agua potable es
+          accesible de forma permanente”.
         </p>
       </div>
     ),
