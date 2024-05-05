bee7118e95e2767303e2a8b217ff9a1fa8406f74
Alfonso Garcia <alfonso.garcia@remberg.de>
2024-05-05 10:48:33 +0200

    new  blog post

--- src/constants/news/blog-articles.tsx
+++ src/constants/news/blog-articles.tsx
@@ -18,6 +18,113 @@ const image: CSSProperties = {
 
 export const BLOG_ARTICLES: BlogArticle[] = [
   {
+    id: "pre-mase",
+    title: `"Un paso más en el proyecto PreMase, se conforman los comités de seguimiento"`,
+    imageUrl: "/oan-web-033.jpg",
+    description: "Álvaro Pascual",
+    date: "1 mayo 2024",
+    link: "/news/pre-mase",
+    author: "Álvaro Pascual",
+    content: (
+      <div style={wrapper}>
+        <p>
+          El 26 de abril de 2024 tuvo lugar en Nikki la formación de los
+          miembros de los comités de seguimiento del proyecto PreMaSe.
+        </p>
+
+        <p>
+          Tras el éxito logrado con la intervención en 10 pueblos durante las
+          fase I y fase II del proyecto PreMase que trabaja para la erradicación
+          de la malnutrición infantil en las zonas rurales de la comuna de
+          Nikki, se da un nuevo paso con la creación de comités de seguimiento
+          en todos esos pueblos.
+        </p>
+
+        <p>
+          Un comité de seguimiento tiene como función monitorear la desnutrición
+          infantil y promover una alimentación saludable en su pueblo,
+          desempeñando un papel crucial en la mejora de la salud de la
+          comunidad.
+        </p>
+
+        <p>
+          Cada comité está compuesto por tres personas de la propia población,
+          elegidas por su buen entendimiento con todos los vecinos. Su trabajo
+          se basará en identificar y derivar los casos individuales de MAM
+          (malnutrición aguda moderada) y MAS (malnutrición aguda severa), y
+          también promoverán cambios a nivel comunitario para mejorar la salud
+          nutricional general.
+        </p>
+
+        <p>
+          La colaboración con las autoridades locales (relais comunitaires), los
+          profesionales de la salud y la comunidad es esencial para el éxito de
+          estas intervenciones. Estos voluntarios recibirán honorarios de
+          motivación (1500F) y costos de comunicación (1500F), por comité,
+          dependiendo del número de casos.
+        </p>
+
+        <p>Estos son los roles que desempeñarán:</p>
+
+        <ul>
+          <li>
+            Identificación y registro de casos de desnutrición en niños de 6
+            meses a 59 meses en colaboración con los relais comunitaires,
+            centros de salud y comunidades locales.
+          </li>
+          <li>Evaluación y seguimiento individual</li>
+          <li>
+            Seguimiento de indicadores de salud: peso, talla, circunferencia del
+            brazo e ingesta de alimentos, para evaluar el progreso y la eficacia
+            de las intervenciones.
+          </li>
+          <li>Realizar evaluaciones periódicas de los niños desnutridos.</li>
+          <li>
+            Establecer planes de seguimiento individualizados y ajustados según
+            sea necesario.
+          </li>
+          <li>
+            Coordinación con el equipo de Nutrición del OAN para garantizar un
+            tratamiento eficaz y un seguimiento médico adecuado (supervisión
+            conjunta de un trabajador sanitario)
+          </li>
+          <li>
+            Promover la lactancia materna y brindar apoyo a las madres
+            lactantes. (sensibilización)
+          </li>
+          <li>
+            Asesoramiento y apoyo psicosocial a las familias, para recordar los
+            factores emocionales y sociales vinculados a la desnutrición.
+          </li>
+          <li>
+            Fomentar la participación comunitaria en la toma de decisiones
+            relacionadas con la salud y la nutrición.
+          </li>
+          <li>
+            Red de apoyo social: Establecer y fortalecer redes de apoyo social
+            para compartir recursos, información y experiencias entre familias.
+          </li>
+          <li>
+            Monitorear los recursos alimentarios y evaluar la disponibilidad y
+            accesibilidad de alimentos nutritivos en la comunidad.
+          </li>
+          <li>
+            Realización de informes para registrar avances, desafíos y
+            recomendaciones para mejorar las intervenciones.
+          </li>
+        </ul>
+
+        <Image
+          style={image}
+          width="300"
+          height="300"
+          src="/oan-web-033.jpg"
+          alt=""
+        />
+      </div>
+    ),
+  },
+  {
     id: "conferencia-del-karite",
     title: `"Conferencia del Karité en Nikki"`,
     imageUrl: "/oan-web-004.jpg",
