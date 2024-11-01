8a64af714916ee72a7852ece637658ab21324443
Alfonso Garcia <alfonso.garcia@remberg.de>
2024-11-01 15:40:23 +0100

    new blog post

--- src/constants/news/blog-articles.tsx
+++ src/constants/news/blog-articles.tsx
@@ -18,6 +18,75 @@ const image: CSSProperties = {
 
 export const BLOG_ARTICLES: BlogArticle[] = [
   {
+    id: "10-aniversario",
+    imageUrl: "/oan-web-043.jpg",
+    description: "Álvaro Pascual",
+    author: "Álvaro Pascual",
+    date: "26 de octubre",
+    link: "/news/10-aniversario",
+    title:
+      "Celebramos los 10 años de OAN International reuniendo a nuestros colaboradores a lo largo de la década",
+    content: (
+      <div style={wrapper}>
+        <p>
+          El sábado 26 de octubre, la familia OAN International nos reunimos
+          para celebrar los 10 años de nuestra ONGD. Más de 100 personas pudimos
+          conocer los avances y el recorrido de toda una década de proyectos
+          emocionantes que han permitido mejorar la vida de los habitantes de
+          Nikki.
+        </p>
+
+        <p>
+          Tras conocer la realidad de Nikki en 2013, Dani había acumulado
+          infinidad de ideas y ganas de seguir apoyando a la comunidad, así que
+          comenzó a buscar cómplices que le acompañasen en su objetivo. El 21 de
+          julio de 2014, OAN International se creó oficialmente con la firma del
+          acta fundacional por un equipo de 8 miembros fundadores: Daniel,
+          Álvaro, Íñigo, Ricardo, Tamar, Brais, Alejandro y Miguel.
+        </p>
+
+        <p>
+          Diez años más tarde, OAN International se ha convertido en 2024 en una
+          ONGD consolidada en la que ya trabajan un grupo de voluntarios en
+          España junto a 10 compañeros contratados en Benín. Hasta hoy, más de
+          60 trabajos de fin de grado y fin de máster han formado parte del
+          proyecto, y más de 40.000 personas han sido beneficiarias en el último
+          año.
+        </p>
+
+        <p>
+          El evento constituyó el reencuentro entre voluntarios de España,
+          colaboradores, profesionales del sector e interesados. Además, tuvimos
+          ocasión de conectar por videollamada con los compañeros de Nikki para
+          que, en primera persona, nos contaran su experiencia y su trabajo.
+        </p>
+
+        <p>
+          Después, pudimos disfrutar de un espacio de celebración compartiendo
+          charlas, visitando la exposición fotográfica, conociendo Sansi Gandó o
+          Besangorou a través de una experiencia 360º o adquiriendo productos
+          Nikarit en nuestro stand solidario.
+        </p>
+
+        <p>
+          Este hito en nuestra trayectoria supone un impulso para seguir
+          avanzando y acompañando a la población de Nikki en la defensa de sus
+          derechos, trabajando en seguridad alimentaria, garantizando el acceso
+          a agua potable, la protección de los derechos de la mujer y la
+          preservación del medioambiente.
+        </p>
+
+        <Image
+          style={image}
+          width="300"
+          height="300"
+          src="/oan-web-043.jpg"
+          alt=""
+        />
+      </div>
+    ),
+  },
+  {
     id: "conferencia-anual-de-karite",
     imageUrl: "/oan-web-041.jpg",
     description: "Álvaro Pascual",
