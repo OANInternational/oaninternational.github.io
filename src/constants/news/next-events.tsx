911b382e3b8fcb45eac363fa9a1cf810260e13ef
Alfonso Garcia Hernandez <53443589+fonss95@users.noreply.github.com>
2025-12-07 22:11:53 +0100

    update deps

--- src/constants/news/next-events.tsx
+++ src/constants/news/next-events.tsx
@@ -1,15 +1,15 @@
 import { Event } from "@/components/event-preview/event-preview.component";
 
 export const NEXT_EVENTS: Event[] = [
-  {
-    title: "Las novedades de OAN International - 11º Aniversario",
-    imageUrl: "/oan-web-043.jpg",
-    description:
-      "¡Encuentro anual! Conoce novedades de los proyectos, acércate más a la realidad de Nikki y disfruta con nosotros. Celebra con nosotros nuestro 11º aniversario en Talent Garden Madrid.",
-    date: "29 de noviembre 2025, 18:00",
-    price: "Donación",
-    ticketUrl: "https://www.eventbrite.es/e/entradas-las-novedades-de-oan-international-1758692770419",
-  },
+  // {
+  //   title: "Las novedades de OAN International - 11º Aniversario",
+  //   imageUrl: "/oan-web-043.jpg",
+  //   description:
+  //     "¡Encuentro anual! Conoce novedades de los proyectos, acércate más a la realidad de Nikki y disfruta con nosotros. Celebra con nosotros nuestro 11º aniversario en Talent Garden Madrid.",
+  //   date: "29 de noviembre 2025, 18:00",
+  //   price: "Donación",
+  //   ticketUrl: "https://www.eventbrite.es/e/entradas-las-novedades-de-oan-international-1758692770419",
+  // },
   // {
   //   title: "Magia en por Benin",
   //   imageUrl:
